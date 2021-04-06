e8400b4d9195048d53dd6bcfe37e2af7d57820e0
Dhruv <Dhruv>
2021-04-06 12:28:18 +0400

    Week_04 Question2

---
+++ Week_04/Question2.cpp
@@ -0,0 +1,8 @@
+/* 
+ * what you have to do
+ * create a circularqueue for keeping a record of all the positive integers
+ * eg 2,3, 4,5,(from 2) 6,7 from(3)
+ * */
+class circularqueue{
+	public:
+};
